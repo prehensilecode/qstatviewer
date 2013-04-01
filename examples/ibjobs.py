018c9f9402d9e90315f3eeead4813ba6cfe8db22
David Chin <david.w.h.chin@gmail.com>
2013-04-01 18:10:49 -0400

    Update ibjobs.py example

--- examples/ibjobs.py
+++ examples/ibjobs.py
@@ -19,21 +19,18 @@ group = ''
 reqwt = ''
 usewt = ''
 
-formatstr = "{n:<8}: {id:>6} {owner:>8} {group:>14}   {reqwt:>10}  {usewt:>10}"
+formatstr = "{n:>9}: {id:<10} {owner:>8} {group:>14}   {reqwt:>10}  {usewt:>10}"
 print(formatstr.format(n="NODE", id="JOBID", owner="OWNER", group="GROUP", reqwt="REQ.WALL", usewt="CUR.WALL"))
-print("-------------------------------------------------------------------")
+print("----------------------------------------------------------------------")
 for nodename,node in ibnodes.iteritems():
     if node.unique_jobs:
-        #print("{0}:".format(nodename))
-        #print("    {0}".format(node.unique_jobs))
         for j in node.unique_jobs:
             id = q.jobs[j].id.split('.')[0]
             owner = q.jobs[j].owner
             group = q.jobs[j].group
             reqwt = q.jobs[j].resource_list['walltime']
             usewt = q.jobs[j].resources_used['walltime']
-            print(formatstr.format(n=nodename, id=id, owner=owner, group=group, reqwt=reqwt, usewt=usewt))
-        #for k,v in node.__dict__.iteritems():
-        #    print("    {0:11.11}\t\t{1}".format(k, v))
-        #print("\n")
+            n_nodes = int(q.jobs[j].resource_list['nodes'].split(':')[0])
+            if n_nodes == 1:
+                print(formatstr.format(n=nodename, id=id, owner=owner, group=group, reqwt=reqwt, usewt=usewt))
 
