21b757a5f50ba8fa1217d802f1e287335458b72a
David Chin <david.w.h.chin@gmail.com>
2013-04-08 10:11:28 -0400

    Output format change. Copyright

--- examples/ibjobs.py
+++ examples/ibjobs.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
+"""
+Prints out single-node jobs requesting walltime >48 hrs running on Infiniband
+"""
 
 # Author: David Chin <dwchin . acm.org>
-
-# qstatviewer by David Chin is licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
-# http://creativecommons.org/licenses/by-sa/3.0/deed.en_US
+# Copyright 2013 Wake Forest University
 
 import sys
 import os
+import datetime
 import qstatviewer as qv
 
 q = qv.QstatViewer()
@@ -19,18 +21,21 @@ group = ''
 reqwt = ''
 usewt = ''
 
-formatstr = "{n:>9}: {id:<10} {owner:>8} {group:>14}   {reqwt:>10}  {usewt:>10}"
+formatstr = "{n:<8} | {id:>6} | {owner:>8} | {group:>14} |  {reqwt:>13.13} |  {usewt:>13.13}"
 print(formatstr.format(n="NODE", id="JOBID", owner="OWNER", group="GROUP", reqwt="REQ.WALL", usewt="CUR.WALL"))
-print("----------------------------------------------------------------------")
-for nodename,node in ibnodes.iteritems():
+print("---------+--------+----------+----------------+----------------+---------------")
+for nodename,node in sorted(ibnodes.iteritems()):
     if node.unique_jobs:
         for j in node.unique_jobs:
             id = q.jobs[j].id.split('.')[0]
             owner = q.jobs[j].owner
             group = q.jobs[j].group
             reqwt = q.jobs[j].resource_list['walltime']
-            usewt = q.jobs[j].resources_used['walltime']
-            n_nodes = int(q.jobs[j].resource_list['nodes'].split(':')[0])
-            if n_nodes == 1:
-                print(formatstr.format(n=nodename, id=id, owner=owner, group=group, reqwt=reqwt, usewt=usewt))
+            t = [int(i) for i in reqwt.split(':')]
+            td = datetime.timedelta(hours=t[0], minutes=t[1], seconds=t[2])
+            if td > datetime.timedelta(hours=48):
+                usewt = qv.timedeltastr(q.jobs[j].resources_used['walltime'])
+                n_nodes = int(q.jobs[j].resource_list['nodes'].split(':')[0])
+                if n_nodes == 1:
+                    print(formatstr.format(n=nodename, id=id, owner=owner, group=group, reqwt=qv.timedeltastr(reqwt), usewt=usewt))
 
