652bac5944bae651248b586cb043dcf2aa8f86a1
David Chin <david.w.h.chin@gmail.com>
2013-04-17 10:59:50 -0400

    Another example script, using PBSQuery

---
+++ examples/pbsquerynodes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+
+from PBSQuery import PBSQuery
+
+def main():
+    pq = PBSQuery()
+    nodes = pq.getnodes()
+    for k,v in sorted(nodes.iteritems()):
+        print v['state']
+    
+
+if __name__ == '__main__':
+    main()
+
