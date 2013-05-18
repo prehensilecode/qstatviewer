1c401bf8bf473bf34ced146b177bd3f9cb378243
David Chin <david.w.h.chin@gmail.com>
2013-05-17 20:59:22 -0400

    Example script to print torque queues

---
+++ examples/pqqueues.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import sys
+import os
+import re
+
+from PBSQuery import PBSQuery
+
+def main():
+    pq = PBSQuery()
+    queuedict = pq.getqueues()
+    for queuename, queue in sorted(queuedict.iteritems()):
+        print queuename
+        for k, v in queue.iteritems():
+            print k, v
+        print
+
+if __name__ == '__main__':
+    main()
+
